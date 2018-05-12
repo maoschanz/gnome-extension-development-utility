f443d877ea467228d553a20915f2a81ce933aebb
Romain F. T <rrroschan@gmail.com>
2018-05-12 16:03:35 +0200

    new simpler version

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 ## GNOME Shell Extension Development Utility
 
->2018-04-24
-
-This extension is a fork of "Gnome Shell Extension Reloader", an
-excellent extension by Norman L. Smith you can find [here](https://github.com/nls1729/acme-code/tree/master/extension-reloader).
-
-This extension is intended for use by Gnome Shell Extension writers. It is common practice to restart the Shell during testing to reload an extension with changes made to the extension's code. Wayland does not allow restarting the Shell. To reload an extension under Wayland a logout and a login is required. "Gnome Shell Extension Reloader" reloads only the selected extension with two mouse clicks saving time for the extension writer. **This doesn't work for GS > 3.25**
-
-It also provides an easy access to features such as:
+This extension provides an easy access to features such as:
 
 - Reloading GNOME Shell
-- Trigger the garbage collector
-- Open the extensions folder
 - Looking Glass
-- GS logs
+- See GNOME Shell logs
+- Extension preferences
 
-Available in French and approximative English
+### TODO
+
+translations (for accessibility)
+
+customize available buttons ?
--- extension.js
+++ extension.js
@@ -1,448 +1,137 @@
-/*
-  Extension Development Utility
-
-  Copyright (c) 2016 Norman L. Smith, Romain F. T.
-
-  This extension is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This extension is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this extension. If not, see
-  < https://www.gnu.org/licenses/old-licenses/gpl-2.0.html >.
-
-  This extension is a derived work of the Gnome Shell.
-
-  This extension is intended for use by Gnome Shell Extension writers.
-  It is common practice to restart the Shell during extension testing
-  to reload the extension to test changes made to the extension's code.
-  Wayland does not allow restart of the Shell. To reload an extension
-  under Wayland a logout and a login is required. The extension reloads
-  only the selected extension with two mouse clicks saving time for the
-  extension writer.
-*/
 
 const Lang = imports.lang;
-const Mainloop = imports.mainloop;
-const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
+const Panel = imports.ui.panel;
+const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
 const Meta = imports.gi.Meta;
+const PopupMenu = imports.ui.popupMenu;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const Shell = imports.gi.Shell;
-const St = imports.gi.St;
-const ExtensionSystem = imports.ui.extensionSystem;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Panel = imports.ui.panel;
-const ExtensionUtils = imports.misc.extensionUtils;
 const Util = imports.misc.util;
+const Slider = imports.ui.slider;
+
+const GTop = imports.gi.GTop; //gir1.2-gtop-2.0
+
+const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Notify = Me.imports.notify;
-const System = imports.system;
+const Convenience = Me.imports.convenience;
 
 const Gettext = imports.gettext.domain('extension-development-utility');
 const _ = Gettext.gettext;
-const Convenience = Me.imports.convenience;
-
-const ENABLED = 1;
-const ICON = [	'dialog-information-symbolic',
-				'dialog-warning-symbolic',
-				'dialog-error-symbolic'
-				];
-const MAX_HEIGHT = parseInt(global.screen_height * 0.95).toString();
-const ROLE = 'extension-reloader-indicator';
-let STATE;
-const STYLE1 = 'width: 120px;';
-const STYLE2 = 'font-weight: bold;';
-const TYPE = {	info: 0,
-				warning: 1,
-				error: 2
-			};
 
 const TERMINAL_SCHEMA = 'org.gnome.desktop.default-applications.terminal';
 const EXEC_KEY = 'exec';
 const EXEC_ARG_KEY = 'exec-arg';
 
-const SubMenuItem = new Lang.Class({
-	Name: 'ExtensionDevelopmentUtility.SubMenuItem',
+//-------------------------------------------------
+
+function init() {
+	Convenience.initTranslations();
+}
+
+//---------------------------------
+
+const ExtensionsButtonsItem = new Lang.Class({
+	Name: 'ExtensionsButtonsItem',
 	Extends: PopupMenu.PopupBaseMenuItem,
 
-	_init: function(extension, name, menu, subMenu) {
-	this.parent();
-		this._extension = extension;
-		this._state = extension.state;
-		this._uuid = extension.uuid;
-		this._name = name;
-		if (this._state > 6)
-			this._state = 0;
-		let box = new St.BoxLayout();
-		let label1 = new St.Label({ text: STATE[this._state] });
-		label1.set_style(STYLE1);
-		box.add_actor(label1);
-		let label2 = new St.Label({ text: name });
-		if (this._state == ENABLED)
-			label2.set_style(STYLE2);
-		box.add_actor(label2);
-		this.actor.add_child(box);
-		this._subMenu = subMenu;
-		this._menu = menu;
-		this._keyInId = 0;
-	},
-
-	destroy: function() {
-		this.actor.disconnect(this._keyInId);
-		this.parent();
-	},
-
-	activate: function() {
-		let enabledExtensions = global.settings.get_strv(ExtensionSystem.ENABLED_EXTENSIONS_KEY);
-		if (enabledExtensions.indexOf(this._uuid) == -1) {
-			enabledExtensions.push(this._uuid);
-			global.settings.set_strv(ExtensionSystem.ENABLED_EXTENSIONS_KEY, enabledExtensions);
-		}
-		try {
-			ExtensionSystem.reloadExtension(this._extension);
-			
-			Notify.notify(_("Reloading completed"), this._name, TYPE.info);
-			log("Reloading completed" + ' : ' + this._name + ' : ' + this._uuid);
-		} catch(e) {
-			Notify.notify(_("Error reloading") + ' : ' + this._name, e.message + ' : ' + this._uuid, TYPE.error);
-		}
-		this._subMenu.close();
-		this._menu.close();
-	}
-});
-
-const ExtensionDevelopmentUtilityMenu = new Lang.Class({
-	Name: 'ExtensionDevelopmentUtility.ExtensionDevelopmentUtilityMenu',
-	Extends: PanelMenu.Button,
-
-	_init: function(subMenuIsOpen) {
-		this.parent(0.5, 'Extension Development Utility Menu');
-		let hbox = new St.BoxLayout({
-			style_class: 'panel-status-menu-box'
-		});
-		let iconBin = new St.Bin();
-		iconBin.child = new St.Icon({
-			icon_name: 'system-run-symbolic',
-			style_class: 'system-status-icon'
-		});
-		hbox.add_child(iconBin);
-	//	hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
-		this._timeoutId = 0
-		this.actor.add_actor(hbox);
+	_init: function() {
+		
+		this.parent({ reactive: false, can_focus: false });
 		
 		this._terminalSettings = new Gio.Settings({ schema_id: TERMINAL_SCHEMA });
 		
-		this.lookingGlass = new PopupMenu.PopupMenuItem("Looking Glass");
-		this.menu.addMenuItem(this.lookingGlass);
-		this.lookingGlass.connect('activate', Lang.bind(this, this._onLookingGlass));
+		this.prefsButton = new St.Button({
+			reactive: true,
+			can_focus: true,
+			track_hover: true,
+			accessible_name: _("Extensions preferences"),
+			style_class: 'system-menu-action',
+		});
+		this.prefsButton.child = new St.Icon({ icon_name: 'system-run-symbolic' });
 		
-		this.seeLogs = new PopupMenu.PopupMenuItem(_("See logs"));
-		this.menu.addMenuItem(this.seeLogs);
-		this.seeLogs.connect('activate', Lang.bind(this, this._onSeeLogs));
-		//----------------------------------------------
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		//----------------------------------------------
-
-		this.garbageCollector = new PopupMenu.PopupMenuItem(_("Collect garbage"));
-		this.menu.addMenuItem(this.garbageCollector);
-		this.garbageCollector.connect('activate', Lang.bind(this, this._onGC));
+		this.seeLogsButton = new St.Button({
+			reactive: true,
+			can_focus: true,
+			track_hover: true,
+			accessible_name: _("See logs"),
+			style_class: 'system-menu-action',
+		});
+		this.seeLogsButton.child = new St.Icon({ icon_name: 'utilities-terminal-symbolic' });
 		
-//		this.higherPriority = new PopupMenu.PopupMenuItem(_("Give high priority"));
-//		this.menu.addMenuItem(this.higherPriority);
-//		this.higherPriority.connect('activate', Lang.bind(this, this._onHigherPriority));
-
-		this.kill = new PopupMenu.PopupMenuItem(_("Kill current app"));
-		this.menu.addMenuItem(this.kill);
-		this.kill.connect('activate', Lang.bind(this, this._onKill));
-
-		//----------------------------------------------
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		//----------------------------------------------
-
-		// restart
-		this.restart = new PopupMenu.PopupMenuItem(_("Restart GNOME Shell"));
-		this.menu.addMenuItem(this.restart);
-		if(!Meta.is_wayland_compositor()){
-			this.restart.connect('activate', Lang.bind(this, this._onRestart));
-		} else {
-			this.restart.setSensitive(false);
-		}
-
-		// reload theme
-		this.reloadTheme = new PopupMenu.PopupMenuItem(_("Reload Theme"));
-		this.menu.addMenuItem(this.reloadTheme);
-		this.reloadTheme.connect('activate', Lang.bind(this, this._onReloadTheme));
-
-		//----------------------------------------------
+		this.restartButton = new St.Button({
+			reactive: true,
+			can_focus: true,
+			track_hover: true,
+			accessible_name: _("Reload GNOME Shell"),
+			style_class: 'system-menu-action',
+		});
+		this.restartButton.child = new St.Icon({ icon_name: 'view-refresh-symbolic' });
 		
-		this.reloadAllExtensions = new PopupMenu.PopupMenuItem(_("Reload All Extensions"));
-		this.menu.addMenuItem(this.reloadAllExtensions);
-		this.reloadAllExtensions.connect('activate', Lang.bind(this, this._onReloadAll));
+		this.lgButton = new St.Button({
+			reactive: true,
+			can_focus: true,
+			track_hover: true,
+			accessible_name: _("Looking Glass"),
+			style_class: 'system-menu-action',
+		});
+		this.lgButton.child = new St.Icon({ icon_name: 'system-search-symbolic' });
 		
-		//----------------------------------------------
+		this.actor.add(this.prefsButton, { expand: true, x_fill: false });
+		this.actor.add(this.seeLogsButton, { expand: true, x_fill: false });
+		this.actor.add(this.restartButton, { expand: true, x_fill: false });
+		this.actor.add(this.lgButton, { expand: true, x_fill: false });
 		
-		let reloadExtension = _("Restart An Extension");
-		this._subMenuMenuItem = new PopupMenu.PopupSubMenuMenuItem(reloadExtension, false);
-		this.menu.addMenuItem(this._subMenuMenuItem);
-
-		if (subMenuIsOpen) {
-			this._openToggledId = this.menu.connect('open-state-changed', Lang.bind(this, this._openToggled));
-		} else {
-			this._openToggledId = this.menu.connect('open-state-changed', Lang.bind(this, function(){
-				this._subMenuMenuItem.setSubmenuShown(false);
-			}));
-		}
-
-		this._scrollView = this._subMenuMenuItem.menu.actor;
-		this._vBar = this._subMenuMenuItem.menu.actor.get_vscroll_bar();
-		this._vBar.vscrollbar_policy = true;
-		this._populateSubMenu(this._subMenuMenuItem.menu);
-		
-		//----------------------------------------------
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		//----------------------------------------------
-		
-		// open extensions folder
-		this.ef = new PopupMenu.PopupMenuItem(_("Open Local Extensions Folder"));
-		this.menu.addMenuItem(this.ef);
-		this.ef.connect('activate', Lang.bind(this, this._doLocalFolder));
-		
-		// open extensions folder
-		this.ew = new PopupMenu.PopupMenuItem(_("Manage Extensions"));
-		this.menu.addMenuItem(this.ew);
-		this.ew.connect('activate', Lang.bind(this, this._doLocalExtensions));
+		this.lgButton.connect('clicked', Lang.bind(this, this._openLookingGlass));
+		this.prefsButton.connect('clicked', Lang.bind(this, this._openPrefs));
+		this.restartButton.connect('clicked', Lang.bind(this, this._reloadGS));
+		this.seeLogsButton.connect('clicked', Lang.bind(this, this._seeLogs));
 	},
 	
-		
-	_doLocalFolder: function() {
-		Main.Util.trySpawnCommandLine('xdg-open .local/share/gnome-shell/extensions/');
-	},
-
-	_doLocalExtensions: function() {
-		Main.Util.trySpawnCommandLine('xdg-open https://extensions.gnome.org/local/');
+	_openPrefs: function() {
+		Util.trySpawnCommandLine('gnome-shell-extension-prefs');
+		Main.panel.statusArea.aggregateMenu.menu.close();
 	},
 	
-	_onReloadTheme: function() {
-		Main.loadTheme();
-	},
-	
-	_onLookingGlass: function() {
+	_openLookingGlass: function() {
 		Main.createLookingGlass().toggle();
+		Main.panel.statusArea.aggregateMenu.menu.close();
 	},
 	
-	_onGC: function() {
-		System.gc();
-	},
-
-	_onKill: function() {
-		let pid = Shell.WindowTracker.get_default().focus_app.get_windows()[0].get_pid();
-		Util.trySpawnCommandLine(
-			'kill -9 ' + pid
-		);
-	},
-
-	_onHigherPriority: function() {
-		let exec1 = this._terminalSettings.get_string(EXEC_KEY);
-		let exec_arg = this._terminalSettings.get_string(EXEC_ARG_KEY);
-		let [pid] = GLib.spawn_command_line_sync('pidof gnome-shell');
-
-		let command = exec1 + ' ' + exec_arg + ' ' + 'pkexec renice -n -5 -p ' + pid; //TODO
-		Util.trySpawnCommandLine(command);
-	},
-	
-	_onSeeLogs: function() {
-		let exec1 = this._terminalSettings.get_string(EXEC_KEY);
-		let exec_arg = this._terminalSettings.get_string(EXEC_ARG_KEY);
-		let command = exec1 + ' ' + exec_arg + ' pkexec journalctl -f /usr/bin/gnome-shell';
-		Util.trySpawnCommandLine(command);
-	},
-	
-	_onRestart: function() {
-		//global.reexec_self();
+	_reloadGS: function() {
+		Main.panel.statusArea.aggregateMenu.menu.close();
 		if (Meta.is_wayland_compositor()) {
 			this._showError(_("Restart is not available on Wayland"));
 			return;
 		}
 		Meta.restart(_("Restarting…"));
 	},
-
-	_openToggled: function(menu, open) {
-		if (open) {
-			this._subMenuMenuItem.menu.removeAll();
-			this._populateSubMenu(this._subMenuMenuItem.menu);
-			this._subMenuMenuItem.menu.open();
-		}
+	
+	_seeLogs: function() {
+		let exec1 = this._terminalSettings.get_string(EXEC_KEY);
+		let exec_arg = this._terminalSettings.get_string(EXEC_ARG_KEY);
+		let command = exec1 + ' ' + exec_arg + ' pkexec journalctl -f /usr/bin/gnome-shell';
+		Util.trySpawnCommandLine(command);
+		Main.panel.statusArea.aggregateMenu.menu.close();
 	},
 	
-	_onReloadAll: function() {
-		this._timeoutId = Mainloop.timeout_add(3000, Lang.bind(this, this._delayedReload));
-	},
-	
-	_delayedReload: function() {
-		if (this._timeoutId != 0) {
-			Mainloop.source_remove(this._timeoutId);
-			this._timeoutId = 0;
-		}
-		
-		let allExtensions = new Gio.Settings({
-			schema_id: 'org.gnome.shell'
-		});
-		allExtensions.set_boolean('disable-user-extensions', true);
-		allExtensions.set_boolean('disable-user-extensions', false);
-	},
-
-	_scrollMenuBox: function(actor) {
-		let box = actor.get_allocation_box();
-		let currentValue = this._vBar.get_adjustment().get_value();
-		let newValue = currentValue;
-		let delta = Math.ceil((box.y2 - box.y1) * .25);
-		if (currentValue > (box.y1 - delta))
-			newValue = box.y1 - delta;
-		if ((this._scrollView.height + currentValue) < (box.y2 + delta))
-			newValue = box.y2 - this._scrollView.height + delta;
-		if (newValue != currentValue)
-			this._vBar.get_adjustment().set_value(newValue);
-	},
-
-	_compare: function(a, b) {
-		let aKey = a.state.toString() + a.metadata.name.toUpperCase();
-		let bKey = b.state.toString() + b.metadata.name.toUpperCase();
-		return (aKey > bKey) ? 0 : -1;
-	},
-
-	_populateSubMenu: function(subMenu) {
-		let sortedArray = [];
-		for (let i in ExtensionUtils.extensions) {
-			let entry = ExtensionUtils.extensions[i];
-			Util.insertSorted(sortedArray, entry, Lang.bind(this, function(a, b) {
-				return this._compare(a, b);
-			}));
-		}
-		for (let i in sortedArray) {
-			let uuid = sortedArray[i].uuid;
-			let name = sortedArray[i].metadata.name;
-			let state = sortedArray[i].state;
-			let ext = sortedArray[i];
-			let item = new SubMenuItem(ext, name, this.menu, subMenu);
-			item._keyInId = item.actor.connect('key-focus-in', Lang.bind(this, this._scrollMenuBox));
-			subMenu.addMenuItem(item);
-		}
-	},
-
-	destroy: function() {
-	
-		if (this._timeoutId != 0) {
-			Mainloop.source_remove(this._timeoutId);
-			this._timeoutId = 0;
-		}
-		
-		this.menu.disconnect(this._openToggledId);
-		this._subMenuMenuItem.menu.removeAll();
-		this.menu.removeAll();
-		this.parent();
-	}
 });
 
-const ExtensionDevelopmentUtilityExtension = new Lang.Class({
-	Name: 'ExtensionDevelopmentUtility.ExtensionDevelopmentUtilityExtension',
+let my_section;
 
-	_init: function() {
-		this._button = null;
-		this._timeoutId = 0;
-		let GioSSS = Gio.SettingsSchemaSource;
-		let schema = Me.metadata['settings-schema'];
-		let schemaDir = Me.dir.get_child('schemas').get_path();
-		let schemaSrc = GioSSS.new_from_directory(schemaDir, GioSSS.get_default(), false);
-		let schemaObj = schemaSrc.lookup(schema, true);
-		this._settings = new Gio.Settings({ settings_schema: schemaObj });
-		this._positionChangedSig = 0;
-	},
+function enable() {
+	let aggregateMenu = Main.panel.statusArea.aggregateMenu;
+	
+	my_section = new PopupMenu.PopupMenuSection();
+	my_section.addMenuItem(new ExtensionsButtonsItem());
+	
+	aggregateMenu._extensions = my_section;
+	aggregateMenu.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem, 0);
+	aggregateMenu.menu.addMenuItem(aggregateMenu._extensions, 0);
+}
 
-	_positionChange: function() {
-		this.disable();
-		this._delayedEnable();
-	},
-
-	_getPosition: function() {
-		let positionInt = this._settings.get_int('panel-icon-position');
-		let position;
-		
-		switch(positionInt) {
-			case 0:
-				position = [0, 'center'];
-				break;
-			case 1:
-				position = [-1, 'center'];
-				break;
-			case 2:
-				position = [0, 'right'];
-				break;
-			case 3:
-				position = [-1, 'right'];
-				break;
-			default:
-				position = [0, 'center'];
-				break;
-		}
-		
-		return position;
-	},
-
-	_delayedEnable: function() {
-		if (this._timeoutId != 0) {
-			Mainloop.source_remove(this._timeoutId);
-			this._timeoutId = 0;
-		}
-		
-		let parameter = this._settings.get_boolean('submenu-is-open');
-		this._button = new ExtensionDevelopmentUtilityMenu(parameter);
-		
-		let position = this._getPosition();
-		Main.panel.addToStatusArea(ROLE, this._button, position[0], position[1]);
-		this._positionChangedSig = this._settings.connect('changed::panel-icon-position', Lang.bind(this, this._positionChange));
-	},
-
-	destroy: function() {
-		if (this._button != null) {
-			this._button.destroy();
-			this._button = null;
-		}
-	},
-
-	enable: function() { 
-		if (Main.sessionMode.currentMode == 'user' || Main.sessionMode.currentMode == 'classic') {
-			this._timeoutId = Mainloop.timeout_add(3000, Lang.bind(this, this._delayedEnable));
-		}
-	},
-
-	disable: function() {
-		if (this._timeoutId != 0) {
-			Mainloop.source_remove(this._timeoutId);
-			this._timeoutId = 0;
-		}
-		if (this._button != null) {
-			this._button.destroy();
-			this._button = null;
-		}
-		if (this._positionChangedSig > 0) {
-			this._settings.disconnect(this._positionChangedSig);
-			this._positionChangedSig = 0;
-		}
-	}
-});
-
-function init(metadata) {
-	Convenience.initTranslations();
-	STATE = [_("Unknown"), _("Enabled"), _("Disabled"), _("Error"), _("Out of Date"), _("Downloading"), _("Initialized")];
-	return new ExtensionDevelopmentUtilityExtension();
+function disable() {
+	Main.panel.statusArea.aggregateMenu._extensions.destroy();
 }
 
