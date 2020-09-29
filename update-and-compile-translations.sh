5529cd457f1d8f8cfcd625bdef4e54b96f585bfc
Romain F. T <rrroschan@gmail.com>
2020-09-29 04:09:15 +0200

    Remove line numbers from translations files

--- update-and-compile-translations.sh
+++ update-and-compile-translations.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+ext_name="extension-development-utility"
+ext_id="$ext_name@maestroschan.fr"
+
 #####
 
 echo "Generating .pot file..."
 
-xgettext --files-from=POTFILES.in --from-code=UTF-8 --output=extension-development-utility@maestroschan.fr/locale/extension-development-utility.pot
+xgettext --files-from=POTFILES.in --from-code=UTF-8 --add-location=file --output=$ext_id/locale/$ext_name.pot
 
 #####
 
 IFS='
 '
-liste=`ls ./extension-development-utility@maestroschan.fr/locale/`
-prefix="./extension-development-utility@maestroschan.fr/locale"
+liste=`ls ./$ext_id/locale/`
+prefix="./$ext_id/locale"
 
 for dossier in $liste
 do
-	if [ "$dossier" != "extension-development-utility.pot" ]; then
+	if [ "$dossier" != "$ext_name.pot" ]; then
 		echo "Updating translation for: $dossier"
-		msgmerge -N $prefix/$dossier/LC_MESSAGES/extension-development-utility.po $prefix/extension-development-utility.pot > $prefix/$dossier/LC_MESSAGES/extension-development-utility.temp.po
-		mv $prefix/$dossier/LC_MESSAGES/extension-development-utility.temp.po $prefix/$dossier/LC_MESSAGES/extension-development-utility.po
+		msgmerge -N $prefix/$dossier/LC_MESSAGES/$ext_name.po $prefix/$ext_name.pot > $prefix/$dossier/LC_MESSAGES/$ext_name.temp.po
+		mv $prefix/$dossier/LC_MESSAGES/$ext_name.temp.po $prefix/$dossier/LC_MESSAGES/$ext_name.po
 		echo "Compiling translation for: $dossier"
-		msgfmt $prefix/$dossier/LC_MESSAGES/extension-development-utility.po -o $prefix/$dossier/LC_MESSAGES/extension-development-utility.mo
+		msgfmt $prefix/$dossier/LC_MESSAGES/$ext_name.po -o $prefix/$dossier/LC_MESSAGES/$ext_name.mo
 	fi
 done
 
